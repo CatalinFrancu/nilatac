7985e5584c0c8a36e54a13ef52bf98cb6ebcf04c
cata <cata@ce8a89fd-18e5-4579-b9bd-548ed17656c9>
2014-06-18 09:07:47 +0000

    Add index page.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'smarty3/Smarty.class.php';
+
+$smarty = new Smarty();
+$smarty->template_dir = 'web/templates';
+$smarty->compile_dir = 'web/templates_c';
+$smarty->display('index.tpl');
+
+?>
